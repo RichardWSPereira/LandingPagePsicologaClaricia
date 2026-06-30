8bcf0d9da852479039821003f65768b37497ad82
RICHARD WILLIAM DA SILVA PEREIRA <srichardwilliam@hotmail.com>
2026-06-30 20:32:33 -0300

    Textos anexados
    Anexos do conteúdo da página, estruturando as seções

--- assets/js/dateCurrent.js
+++
@@ -1,7 +0,0 @@
-function meuEscopo() {
-    const dateText = document.querySelector('.titleHTML');
-    const dateCurrent = new Date();  
-    
-    dateText.innerHTML = dateCurrent.toLocaleString('pt-BR',{dateStyle: 'full',timeStyle:'short'});
-}
-meuEscopo();
