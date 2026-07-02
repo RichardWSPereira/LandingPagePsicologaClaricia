2161260a9b89ce3a5df4b8f443bab7e8700c6012
RICHARD WILLIAM DA SILVA PEREIRA <srichardwilliam@hotmail.com>
2026-07-02 18:31:01 -0300

    Mudança da estilização
    Algumas seções tiveram mudanças na estilização e ocorreu o conserto de algumas funcionalidades de button e section

--- assets/js/createtag.js
+++ assets/js/createtag.js
@@ -1,14 +1,14 @@
 const element = [
-    {tag: 'h2', texto: 'Convivência'},
-    {tag: 'p', texto: 'Casais no início do casamento tendem a acreditar que depois do "sim" tudo será um conto de fadas. A vida de casal nesse início é maravilhosa, mas é preciso ter os pés no chão e, claro: estar presente! Para qualquer casamento "dar certo" é preciso entender que haverá dias bons e dias ruins, e que o principal ingrediente de uma vida de casados(as) de sucesso é a dedicação.'},
-    {tag: 'p', texto: 'Saber valorizar as diferenças é algo difícil, mas especialistas afirmam que ninguém consegue mudar muito rapidamente. Nessas horas, ceder um pouco com as próprias opiniões e aceitar as diferenças do seu parceiro ou parceira pode ser o primeiro passo para obter mais harmonia na relação.'},
-    {tag: 'p', texto: 'A individualidade não precisa morrer depois do "sim". Um casal saudável continua a praticar os seus hobbies e suas atividades individuais. .main-contentO espaço de cada um deve permanecer sagrado, e entender que o outro precisa desse espaço é fundamental.'},
+    {tag: 'h4', texto: 'Você sente que está carregando um peso maior do que consegue suportar?'},
+    {tag: 'p', texto: 'Nem sempre é fácil lidar com as emoções, as cobranças do dia a dia, os relacionamentos ou as mudanças da vida. Em muitos momentos, podemos nos sentir sobrecarregados, ansiosos, desmotivados ou com a sensação de que estamos apenas sobrevivendo à rotina.'},
+    {tag: 'p', texto: 'Talvez você esteja enfrentando dificuldades para controlar a ansiedade, lidando com pensamentos que geram sofrimento, vivendo conflitos nos relacionamentos, passando por um momento de insegurança ou simplesmente sentindo que perdeu a conexão consigo mesmo.'},
+    {tag: 'p', texto: 'Você não precisa enfrentar tudo isso sozinho. Buscar ajuda psicológica é um ato de cuidado consigo e uma oportunidade de desenvolver novas formas de compreender suas emoções, fortalecer seus recursos internos e construir uma vida com mais equilíbrio e qualidade.'},
 ];
 
 const sectionTarget = document.querySelector('.createTag');
 const div = document.createElement('div');
 sectionTarget.appendChild(div);
-div.classList.add('conv-content','main-content');
+div.classList.add('porque-content','main-content');
 for (let i = 0; i < element.length; i++) {
     const {tag, texto} = element[i];
     const tagCreated = document.createElement(tag);
